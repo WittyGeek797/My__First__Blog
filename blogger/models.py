d6bd0f132aae77269dad93f11eeffc50e006e67b
Social_OutKast <sagarmanjare797@gmail.com>
2016-01-17 18:45:57 +0530

    Added commenting feature and completed

--- blogger/models.py
+++ blogger/models.py
@@ -15,5 +15,22 @@ class Post(models.Model):
 		self.published_date=timezone.now()
 		self.save()
 
-	def __str__(self):
+	def __unicode__(self):
 		return self.title
+
+class Comment(models.Model):
+	post=models.ForeignKey('blogger.Post',related_name='comments')
+	author=models.CharField(max_length=200)
+	text=models.TextField()
+	created_date=models.DateField(default=timezone.now)
+	approved_comment=models.BooleanField(default=False)
+
+	def approve(self):
+		self.approved_comment=True
+		self.save()
+
+	def __unicode__(self):
+		return self.text
+
+	def approved_comments(self):
+		return self.comments.filter(approved_comment=True)
--- blogger/views.py
+++ blogger/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import redirect
-from .models import Post
+from .models import Post,Comment
 from django.utils import timezone
 from django.shortcuts import render,get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .forms import PostForm
+from django.contrib.auth import logout,login
+from .forms import PostForm,CommentForm
 
 # Create your views here.
 
@@ -59,7 +60,33 @@ def publish(self):
 	self.published_date=timezone.now()
 	self.save()
 
+@login_required
 def post_remove(request,pk):
 	post=get_object_or_404(Post,pk=pk)
 	post.delete()
 	return redirect('post_list')
+
+def add_comment_to_post(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    if request.method=="POST":
+        form=CommentForm(request.POST)
+        if form.is_valid:
+            comment=form.save(commit=False)
+            comment.post=post
+            comment.save()
+            return redirect('blogger.views.post_detail',pk=post.pk)
+    else:
+        form=CommentForm()
+    return render(request,'blogger/add_comment_to_post.html',{'form':form})
+
+@login_required
+def comment_approve(request,pk):
+    comment=get_object_or_404(Comment,pk=pk)
+    comment.approve()
+    return redirect('blogger.views.post_detail',pk=comment.post.pk)
+
+@login_required
+def comment_remove(request,pk):
+    comment=get_object_or_404(Comment,pk=pk)
+    comment.delete()
+    return redirect('blogger.views.post_detail',pk=comment.post.pk)
