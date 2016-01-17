f39b19a76813390745e1619c6d7af1248ee832ee
Social_OutKast <sagarmanjare797@gmail.com>
2016-01-17 19:56:56 +0530

    Moving back to sqlite3

--- djangirls/settings.py
+++ djangirls/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'djangirls.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangirls',
-        'USER': 'Sagar',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
